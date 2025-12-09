3b20f205e900db6ddff211ca6ee31a5532b5a772
Ambroise Cainne <ambroisecainne@gmail.com>
2025-12-09 17:27:09 +0100

    ajot fonction modif articels

---
+++ fonctionStoclArticles.c
@@ -0,0 +1,14 @@
+#include <gtk/gtk.h>
+#include "fonctionStoclArticles.h"
+#include <stdio.h>
+#include <string.h>
+
+
+void update_stock(const char *article_id, int new_quantity){
+    //verif que l'ati exei bien
+
+    //upde la table avec +1
+
+    
+}
+
---
+++ fonctionStoclArticles.h
@@ -0,0 +1,10 @@
+#ifndef STOCLARTICLES_H
+#define STOCLARTICLES_H
+
+//defion des ofnciton pour la gestion du stock ajout supr et modifer avec les triant en bdd ducoup
+
+void update_stock(const char *article_id, int new_quantity);
+
+void update_info_article();
+
+#endif
